4fac9fa2cb9fda33865528ec7e2fc06b7fe69285
Igor vuleta <igor.vuleta@wanko.de>
2019-10-02 16:46:02 +0200

    asd

---
+++ Query.cs
@@ -0,0 +1,40 @@
+﻿using hcgraphqlnew.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace hcgraphqlnew
+{
+    public class Query
+    {
+        private northwindContext _northwindContext;
+        public Query(northwindContext northwindcontext)
+        {
+            _northwindContext = northwindcontext;
+        }
+        public string TableName()
+        {
+            var entityTypes =  _northwindContext.Model.GetEntityTypes().Select(t => t.ClrType).ToList();
+
+            List<object> getNames = new List<object>();
+            foreach (var entity in entityTypes)
+            {
+                var name = entity.Name;
+
+
+                return name;
+                
+            }
+
+
+            return getNames.ToList().ToString();
+            
+        }
+    }
+
+    public class Greetings
+    {
+        public string Hello() => "World";
+    }
+}
---
+++ Types/QueryType.cs
@@ -0,0 +1,17 @@
+﻿using HotChocolate.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hcgraphqlnew.Types
+{
+    public class QueryType : ObjectType<Query>
+    {
+
+        protected override void Configure(IObjectTypeDescriptor<Query> descriptor)
+        {
+            descriptor.Field(t => t.TableName()).Type<QueryType>();
+        }
+    }
+}
